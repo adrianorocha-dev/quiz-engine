ac2b033d12fc71f3070605bb522a54909b99ac30
Adriano Rocha <adriano.olr@gmail.com>
2024-08-04 15:52:02 -0300

    style: improve input field

--- src/app/respond/[quizId]/_components/question-input/input-field.tsx
+++ src/app/respond/[quizId]/_components/question-input/input-field.tsx
@@ -4,6 +4,10 @@ export default function InputField(props: {
   question: Extract<Question, { type: "input" }>;
 }) {
   return (
-    <input className="" type="text" placeholder={props.question.placeholder} />
+    <input
+      className="rounded-md border-2 border-gray-300 px-4 py-2 focus:outline-blue-500"
+      type="text"
+      placeholder={props.question.placeholder}
+    />
   );
 }
