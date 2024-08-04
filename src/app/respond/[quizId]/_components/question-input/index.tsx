570d5b55dae7cafcf83b136ab44989dfddfd5817
Adriano Rocha <adriano.olr@gmail.com>
2024-08-04 15:32:07 -0300

    style: improve one-choice field

--- src/app/respond/[quizId]/_components/question-input/index.tsx
+++ src/app/respond/[quizId]/_components/question-input/index.tsx
@@ -1,6 +1,6 @@
 import type { Question } from "~/server/validators";
 import InputField from "./input-field";
-import SelectField from "./select-field";
+import OneChoiceField from "./one-choice-field";
 import MultiSelectField from "./multi-select-field";
 
 export default function QuestionInput(props: { question: Question }) {
@@ -8,7 +8,7 @@ export default function QuestionInput(props: { question: Question }) {
     case "input":
       return <InputField question={props.question} />;
     case "one-choice":
-      return <SelectField question={props.question} />;
+      return <OneChoiceField question={props.question} />;
     case "multiple-choice":
       return <MultiSelectField question={props.question} />;
   }
---
+++ src/app/respond/[quizId]/_components/question-input/one-choice-field.tsx
@@ -0,0 +1,26 @@
+import { ChevronDownIcon } from "lucide-react";
+import type { Question } from "~/server/validators";
+
+export default function OneChoiceField(props: {
+  question: Extract<Question, { type: "one-choice" }>;
+}) {
+  return (
+    <ul className="flex flex-col gap-4">
+      {props.question.options.map((option) => (
+        <li key={option}>
+          <label className="flex items-center gap-2 rounded-xl border-2 border-gray-200 bg-white p-4 transition-colors has-[:checked]:border-blue-400">
+            <input
+              className="h-4 w-4 appearance-none rounded-full border border-gray-300 bg-white transition-colors checked:border-4 checked:border-blue-500 focus:outline-none forced-colors:appearance-auto"
+              id={`input-${option}`}
+              type="radio"
+              name={props.question.title}
+              value={option}
+            />
+
+            {option}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
